403d45e0d7e711da5295ff6aeeaea3478f26ffc3
syntappz <doddsanater87@gmail.com>
2020-03-07 19:30:38 +0000

    spotify sign in is working

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import React from 'react';
-
-import './App.css';
+import React, { useEffect } from "react";
+import queryString from "query-string";
+import "./App.css";
 
 function App() {
+  useEffect(() => {
+    const parsed = queryString.parse(window.location.search);
+    const accessToken = parsed.access_token;
+
+    fetch("https://api.spotify.com/v1/me", {
+      headers: {
+        Authorization: "Bearer " + accessToken
+      }
+    })
+      .then(res => res.json())
+      .then(data => {
+        console.log(data)
+      });
+  }, []);
+
+  const loginToSpoify = () => {
+    return (window.location = "http://localhost:8888/login");
+  };
   return (
     <div className="App">
-    
+      <button onClick={loginToSpoify}>login to spotify</button>
     </div>
   );
 }
