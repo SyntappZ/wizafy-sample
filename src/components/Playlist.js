415cc5c54e5ed58dec09dd866413a4f5b25af3b4
syntappz <doddsanater87@gmail.com>
2020-05-30 18:41:08 +0100

    shortend playlist title

--- src/components/Playlist.js
+++ src/components/Playlist.js
@@ -2,11 +2,13 @@ import React from "react";
 
 const Playlist = ({ playlist, getPlaylistDetails }) => {
   const { image, title } = playlist;
+
+  const playlistTitle = title.length > 35 ? title.slice(0, 35) + '...' : title
   return (
     <div className="playlist" onClick={() => getPlaylistDetails(playlist)}>
       <img src={image} alt="playlist" />
       <div className="cover">
-        <h3>{title}</h3>
+        <h3>{playlistTitle}</h3>
       </div>
     </div>
   );
